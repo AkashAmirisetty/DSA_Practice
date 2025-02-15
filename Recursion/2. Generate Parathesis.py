9c8aa52f7e916b1ffde5dc36db8ef1ad74d98e68
AkashAmirisetty <amirisettyakash@gmail.com>
2025-02-15 23:44:00 +0530

    New changes

---
+++ Recursion/2. Generate Parathesis.py
@@ -0,0 +1,7 @@
+class Solution:
+    def generateParenthesis(self, n):
+        #your code goes here
+    def function(self, index:int, o:int, c:int, []:list,n:int):
+        if o>n:
+            return 
+       
