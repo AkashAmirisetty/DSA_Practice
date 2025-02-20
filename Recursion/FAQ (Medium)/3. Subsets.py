f1194b666f322ab626dfbb81fb63f846b1500056
AkashAmirisetty <amirisettyakash@gmail.com>
2025-02-20 07:34:09 +0530

    changes made

--- Recursion/FAQ (Medium)/3. Subsets.py
+++ Recursion/FAQ (Medium)/3. Subsets.py
@@ -1,16 +1,13 @@
 class Solution:
     def subsetSums(self, nums):
         ans=[]
-        self.function(0,[],ans,nums,len(nums))
+        self.func(0,0,ans,nums)
         return ans
 
         
-        #your code goes here
-    def function(self, index, lis, ans, nums,n):
-        if index==n:
-            ans.append(sum(lis))
+    def func(self,ind,sums,ans,nums):
+        if ind==len(nums):
+            ans.append(sums)
             return
-        lis.append(nums[index])
-        self.function(index+1, lis,ans,nums,n)
-        lis.pop()
-        self.function(index+1,lis,ans,nums, n)
+        self.func(ind+1,sums+nums[ind],ans,nums)
+        self.func(ind+1,sums,ans,nums)
